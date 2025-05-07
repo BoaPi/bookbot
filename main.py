69bbdcdfa7424ade69318d48b7c2c2d828bbdf97
boapi <tobias.weyer1205@gmail.com>
2025-05-08 00:16:48 +0200

    add function to count words in document

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
        return f.read()
 
+def count_words(text):
+    return len(text.split())
+
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    num_words = count_words(get_book_text("./books/frankenstein.txt"))
+    print(f"{num_words} words found in the document")
 
 main()
