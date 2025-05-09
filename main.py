1cb3e93fc4aede54159026cbef460317b212f677
boapi <tobias.weyer1205@gmail.com>
2025-05-09 13:38:14 +0200

    add usage of cli arguments for book path

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_num_words, get_num_characters, sort_character_count
 
 def get_book_text(path_to_file):
@@ -5,7 +7,11 @@ def get_book_text(path_to_file):
        return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     book = get_book_text(f"./{book_path}")
     num_words = get_num_words(book)
     num_characters = get_num_characters(book)
