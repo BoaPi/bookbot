ee5ea8247fdfbc29e059ea04497c9f248dde05e2
boapi <tobias.weyer1205@gmail.com>
2025-05-09 13:30:52 +0200

    use statistic function to create report

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+from stats import get_num_words, get_num_characters, sort_character_count
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
        return f.read()
 
-def count_words(text):
-    return len(text.split())
-
 def main():
-    num_words = count_words(get_book_text("./books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
+    book_path = "books/frankenstein.txt"
+    book = get_book_text(f"./{book_path}")
+    num_words = get_num_words(book)
+    num_characters = get_num_characters(book)
+    char_count = sort_character_count(num_characters) 
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for c in char_count:
+        if c["char"].isalpha():
+            print(f"{c['char']}: {c['count']}")
+    print("============= END ===============")
 main()
